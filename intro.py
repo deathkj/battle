a7dfeef521a10f8f27ec9fa6db9395112a0f9b31
Kenny Haak <quaffs@live.com>
2017-06-25 19:57:41 -0700

    Updated the intro to include Memus

--- intro.py
+++ intro.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 
-print "I am a memelord. Welcome to my lair. You are playing Battle. Copyrighted 2017."
+print "I am a Memelord named Memus. Welcome to my lair. You are playing Battle. Copyrighted & Reserved 2017."
